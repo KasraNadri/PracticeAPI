ff0d6a2e1ccb96787558d12910a607da4809d6fd
nadri.k <nadri.k@daneshrefah.ir>
2024-10-30 13:00:39 +0330

    Changed Villa to User

---
+++ PracticeAPI_API/Controllers/UserAPIController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticeAPI_API.Models;
+using PracticeAPI_API.Models.Dto;
+
+namespace PracticeAPI_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAPIController : ControllerBase
+    {
+        [HttpGet]
+        public List<UserDto> GetUsers()
+        {
+            return new List<UserDto>
+            {
+                new UserDto
+                {
+                    Id = 1, FirstName = "Kasra", LastName = "Nadri"
+                },
+                new UserDto
+                {
+                    Id = 2, FirstName = "Mohammad", LastName = "Ahmadi"
+                }
+            };
+        }
+    }
+}
--- PracticeAPI_API/Models/Villa.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PracticeAPI_API.Models
-{
-    public class Villa
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }    
-    }
-
-}
